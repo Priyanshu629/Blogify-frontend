d5fc5abe3d021bdc7ed13ec691655e3beb0d2dba
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-07-07 14:16:55 +0530

    add some css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,23 @@ import { logout } from "../utils/auth";
 const Header = () => {
   const { isLoggedIn,setIsLoggedIn,setUserId } = useUser();
 
+  const toggleMenu=(e)=>{
+    let menu = document.getElementById("hamburger")
+
+     if(menu.style.display==="none"){
+      menu.style.display="flex"
+    
+     }else{
+     
+      menu.style.display="none"
+     }
+  }
+
+  const toggleLink=()=>{
+    let menu = document.getElementById("hamburger")
+    menu.style.display="none"
+  }
+  
   return (
     <header>
       <Link to={"/"} className="logo">
@@ -12,15 +29,18 @@ const Header = () => {
       </Link>
 
       <ul>
+      <li>
+        <Link to={"/"} className="link" >Home</Link>
+      </li>
         {isLoggedIn === true ? (
           <>
           <li>
-              <Link to="/add-blog" className="link">
+              <Link to="/add-blog" className="link" >
                 Write Blog
               </Link>
             </li>
             <li>
-              <Link to="/profile" className="link">
+              <Link to="/profile" className="link" >
                 Profile
               </Link>
             </li>
@@ -46,6 +66,49 @@ const Header = () => {
           </>
         )}
       </ul>
+<>     <i onClick={toggleMenu}>Menu</i>
+      <ul id="hamburger" >
+      <li>
+        <Link to={"/"} className="link" onClick={toggleLink}>Home</Link>
+      </li>
+        {isLoggedIn === true ? (
+          <>
+          <li>
+              <Link to="/add-blog" className="link" onClick={toggleLink}>
+                Write Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className="link" onClick={toggleLink}>
+                Profile
+              </Link>
+            </li>
+            <li>
+              <button onClick={()=>{
+                logout(setIsLoggedIn,setUserId)
+                toggleLink()
+              }} id="logout" >
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login" className="link" onClick={toggleLink}>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="link" onClick={toggleLink}>
+                SignUp
+              </Link>
+            </li>
+            
+          </>
+        )}
+      </ul>
+      </>
     </header>
   );
 };
