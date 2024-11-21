d3e3afc45983a23608b0dab1cc29add9e790e817
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-11-21 09:20:16 +0530

    refactor backend URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BLOG_BACKEND_URL="http://localhost:5000/api/v1/blog/"
-export const USER_BACKEND_URL="http://localhost:5000/api/v1/user/"
-// export const BLOG_BACKEND_URL="https://blogify-backend-ur0p.onrender.com/api/v1/blog/"
-// export const USER_BACKEND_URL="https://blogify-backend-ur0p.onrender.com/api/v1/user/"
+//export const BLOG_BACKEND_URL="http://localhost:5000/api/v1/blog/"
+//export const USER_BACKEND_URL="http://localhost:5000/api/v1/user/"
+
+ export const BLOG_BACKEND_URL="https://blogify-backend-ur0p.onrender.com/api/v1/blog/"
+ export const USER_BACKEND_URL="https://blogify-backend-ur0p.onrender.com/api/v1/user/"
